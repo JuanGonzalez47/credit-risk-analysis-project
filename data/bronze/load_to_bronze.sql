733a04d58d3dedfce947ab8a1b83ad541c857892
Felixphz <105894241+Felixphz@users.noreply.github.com>
2025-07-31 20:17:16 -0500

    add files
    broze files

---
+++ data/bronze/load_to_bronze.sql
@@ -0,0 +1,25 @@
+--application_train.csv  
+--application_test.csv  
+--bureau.csv  
+--bureau_balance.csv  
+--credit_card_balance.csv  
+--installments_payments.csv  
+--POS_CASH_balance.csv  
+--previous_application.csv 
+
+-- Abre una nueva consola en la ruta del proyecto:
+
+--Copiar
+
+/*mysql -u root -p --local-infile=1
+SET GLOBAL local_infile = 1;
+SHOW VARIABLES LIKE 'local_infile';
+Debe mostrar: ON */
+
+USE bronze;
+LOAD DATA LOCAL INFILE 'bronze/archivos.csv'
+INTO TABLE previous_application
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
